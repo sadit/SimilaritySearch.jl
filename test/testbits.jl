d8b9c22ed9712cf8020fe5d87f16b01bf337cc0b
Eric S. Tellez <donsadit@gmail.com>
2017-12-02 08:18:59 -0600

    adds testbits; renamed testlaesa to testdistances

---
+++ test/testbits.jl
@@ -0,0 +1,13 @@
+using SimilaritySearch
+using Base.Test
+
+@testset "bit ops" begin
+    u = zero(UInt128)
+    v = setbit(u, 0)
+    w = setbit(u, 1)
+    @test v == 1 && w == 2
+    v = setbit(v, 1)
+    @test v == 3
+    v = resetbit(v, 1)
+    @test v == 1
+end
