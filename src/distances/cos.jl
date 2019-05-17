c6dfce92129e6127566587327acecf4f8c026928
Eric S. Tellez <donsadit@gmail.com>
2019-05-17 08:26:11 -0500

    removes some methods in favor of LinearAlgebra's implementations

--- src/distances/cos.jl
+++ src/distances/cos.jl
@@ -1,24 +1,14 @@
-export dot_similarity, normalize!, cosine_similarity, cosine_distance, angle_distance
+export normalize!, cosine_similarity, cosine_distance, angle_distance
 
-"""
-Computes the dot product between vectors (it may disapear in a near future in favor of LinearAlgebra.dot)
-"""
-function dot_similarity(a::AbstractVector{T}, b::AbstractVector{T})::Float64 where {T <: Real}
-    s = 0.0
-    @fastmath @inbounds @simd for i in eachindex(a)
-        s += a[i] * b[i]
-    end
-
-    s
-end
-
+using LinearAlgebra
+import LinearAlgebra: normalize!
 """
 normalize!
 
 Divides the vector by its norm (in-place operation)
 """
 function normalize!(a::AbstractVector{T}) where {T <: Real}
-    xnorm = sqrt(dot_similarity(a, a))
+    xnorm = sqrt(dot(a, a))
     (xnorm <= eps(T)) && error("A valid vector for cosine's normalize! cannot have a zero norm $xnorm -- vec: $a")
     invnorm = 1.0 / xnorm
     @fastmath @inbounds @simd for i in eachindex(a)
@@ -47,7 +37,7 @@ angle_distance
 Computes the angle between two vectors, it expects normalized vectors (see normalize! method)
 """
 function angle_distance(a::AbstractVector{T}, b::AbstractVector{T})::Float64 where {T <: Real}
-    m = max(-1.0, dot_similarity(a, b))
+    m = max(-1.0, dot(a, b))
     acos(min(1.0, m))
 end
 
@@ -59,5 +49,5 @@ Please use angle_distance if you are expecting a metric function (instead cosine
 alternative whenever the triangle inequality is not needed)
 """
 function cosine_distance(a::AbstractVector{T}, b::AbstractVector{T})::Float64 where {T <: Real}
-    1 - dot_similarity(a, b)
+    1 - dot(a, b)
 end
