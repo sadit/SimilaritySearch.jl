3923370fd772a7cb865b5b4773ea22210216d506
Eric S. Tellez <donsadit@gmail.com>
2025-06-20 11:52:56 -0500

    better ranges for the optimizer

--- src/searchgraph/optbs.jl
+++ src/searchgraph/optbs.jl
@@ -8,10 +8,10 @@ export BeamSearchSpace
 Define search space for beam search autotuning
 """
 @with_kw struct BeamSearchSpace <: AbstractSolutionSpace
-    bsize = 2:8:64
-    Δ = [0.8, 0.9, 1.0, 1.1]                  # this really depends on the dataset, be careful
-    bsize_scale = (s=1.5, p1=0.75, p2=0.75, lower=2, upper=512)  # all these are reasonably values
-    Δ_scale = (s=1.05, p1=0.75, p2=0.75, lower=0.5, upper=1.99)  # that should work in most datasets
+    bsize = 2:2:16
+    Δ = [0.8, 0.9, 0.95, 1.0, 1.05, 1.1]                  # this really depends on the dataset, be careful
+    bsize_scale = (s=1.1, p1=0.75, p2=0.75, lower=2, upper=48)  # all these are reasonably values
+    Δ_scale = (s=1.05, p1=0.75, p2=0.75, lower=0.7, upper=1.7)  # that should work in most datasets
 end
 
 Base.hash(c::BeamSearch) = hash((c.bsize, c.Δ, c.maxvisits))
